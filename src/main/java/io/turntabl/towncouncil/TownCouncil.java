2c7abdf5b487b413cf8302c04e4510b8c782b714
Rume7 <rhume.disi@turntabl.io>
2023-05-03 11:51:32 +0100

    Fixed the TownCouncil class with the 3rd party services.

--- src/main/java/io/turntabl/towncouncil/TownCouncil.java
+++ src/main/java/io/turntabl/towncouncil/TownCouncil.java
@@ -1,21 +1,33 @@
 package io.turntabl.towncouncil;
 
 import io.turntabl.owner.Person;
+import io.turntabl.services.PermitIssuerService;
+import io.turntabl.services.VerificationService;
 import io.turntabl.vehicles.Vehicle;
 import io.turntabl.vehicles.VehicleType;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class TownCouncil {
 
     private List<Person> registeredOwners;
-    private Map<VehicleType, Integer> vehiclesWithPermitMapCount;
+    private final Map<VehicleType, Integer> vehiclesWithPermitMapCount;
     private static int count = 1;
+
+    private VerificationService verificationService;
+    private PermitIssuerService permitIssuerService;
 
     public TownCouncil() {
         this.vehiclesWithPermitMapCount = new HashMap<>();
+    }
+
+    public TownCouncil(VerificationService verificationService, PermitIssuerService permitIssuerService) {
+        this.vehiclesWithPermitMapCount = new HashMap<>();
+        this.verificationService = verificationService;
+        this.permitIssuerService = permitIssuerService;
     }
 
     private static String generatePermitNumber() {
@@ -26,7 +38,7 @@ public class TownCouncil {
 
     // give permit to owners by issuing permit numbers.
     public void issueVehiclePermit(Vehicle vehicle) {
-        List<Person> owners = vehicle.getOwnersList();
+        Set<Person> owners = vehicle.getAllOwners();
         for (Person owner : owners) {
             if (owner.isRegistered()) {
                 vehicle.setParkingPermitNumber(generatePermitNumber());
@@ -36,17 +48,28 @@ public class TownCouncil {
         }
     }
 
-    public boolean issueVehiclePermit(Vehicle vehicle, Person permitRequestor) {
-        if (permitRequestor.isRegistered()) {
-            return false;
-        }
-        List<Person> ownersList = vehicle.getOwnersList();
-        if (!ownersList.contains(permitRequestor)) {
-            return false;
-        }
+    public boolean issueVehiclePermit1(Vehicle vehicle, Person permitRequestor) {
         vehicle.setParkingPermitNumber(generatePermitNumber());
         setVehiclesWithPermitMapCount(vehicle);
         return true;
+    }
+
+    public String issueTruckPermit(Vehicle vehicle, Person permitRequestor) {
+        String permitNumber = generatePermitNumber();
+        vehicle.setParkingPermitNumber(permitNumber);
+        setVehiclesWithPermitMapCount(vehicle);
+        return permitNumber;
+    }
+
+    public String issueVehiclePermit(Vehicle vehicle, Person permitRequestor) {
+        if (!verificationService.verifyPerson(permitRequestor, vehicle)) {
+            return "You are not a vehicle owner";
+        } else {
+            if (vehicle.getType() != VehicleType.TRUCK) {
+                return permitIssuerService.issuePermit(vehicle);
+            }
+            return issueTruckPermit(vehicle, permitRequestor);
+        }
     }
 
     /** Display the number of vehicles types. */
