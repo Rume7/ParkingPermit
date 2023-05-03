a48f3b6867734951bb6cce38c8a8e5ce4a3b49b4
Rume7 <rhume.disi@turntabl.io>
2023-05-03 14:22:48 +0100

    Implemented the equals and hashcode methods in the  MotorBike class.

--- src/main/java/io/turntabl/vehicles/MotorBike.java
+++ src/main/java/io/turntabl/vehicles/MotorBike.java
@@ -1,11 +1,13 @@
 package io.turntabl.vehicles;
 
+import java.util.Objects;
+
 public class MotorBike extends Vehicle {
 
-    private int capacity;
-    private MotorBikeType bikeType;
+    private final int capacity;
+    private final MotorBikeType bikeType;
 
-    public MotorBike(String licencePlate, VehicleType type) {
+    public MotorBike(String licencePlate, int capacity) {
         super(licencePlate, VehicleType.MOTORBIKE);
         if (capacity <= 850) {
             bikeType = MotorBikeType.SCOOTER;
@@ -17,7 +19,21 @@ public class MotorBike extends Vehicle {
 
     @Override
     public double getTotalMonthlyCharge() {
-        return 0;
+        return super.getMonthlyBaseCharge();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MotorBike motorBike = (MotorBike) o;
+        return capacity == motorBike.capacity && bikeType == motorBike.bikeType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), capacity, bikeType);
     }
 
     @Override
